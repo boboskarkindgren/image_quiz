cbb60a13ae92c8301746616097624d2e678c0a15
Bob Oskar Kindgren <113449958+boboskarkindgren@users.noreply.github.com>
2022-11-28 11:58:19 +0100

    copy students array and shuffle it

--- script.js
+++ script.js
@@ -7,6 +7,43 @@ const alt = document.querySelector('#alternatives');
 
 const btn1 = document.querySelector('#btn-1');
 
+/*
+//copy array with map
+const shuffledStudents = students.map(student=> {
+
+	return student;
+
+})
+*/
+
+//Copy array with spread so we don´t destruct the original array
+const shuffledStudents = [...students];
+
+const shuffleArray = (array) => {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		const temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+
+	return array;
+}
+
+shuffleArray(shuffledStudents);
+
 console.log(students);
+console.log(shuffledStudents);
+
+
+
+
+
+
+
+
+
+
+
 
 
